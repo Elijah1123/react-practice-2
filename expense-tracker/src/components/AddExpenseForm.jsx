c1d10675df42ec6803968d8a7c8e47adf9f939b8
Elijah M <elijah.macharia@student.moringaschool.com>
2025-07-26 23:02:36 +0300

    Added-Expense-Form

--- expense-tracker/src/components/AddExpenseForm.jsx
+++ expense-tracker/src/components/AddExpenseForm.jsx
@@ -5,11 +5,26 @@ function AddExpenseForm({onAddExpense}) {
     const [description, setDescription] = useState("");
     const [amount, setAmount] = useState("");
     const [category, setCategory] = useState("");
-}
 
-    const handleSubmit = ( e ) => {
-        e.preventDefault();
-        if (!name || !description || !amount || !category) return;
-        onAddExpense
 
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !description || !amount || !category) return;
+    onAddExpense({name, description, amount: parseFloat(amount), category});
+    setName ("");
+    setDescription("");
+    setAmount("");
+    setCategory("");
+    };
+
+    return(
+        <form onSubmit={handleSubmit}   className="form" >
+            <input type="text" placeholder="Expense Name" value={name} onChange={(e) => setName(e.target.value)  }  />
+            <input type="text " placeholder="Description" value={description} onChange={(e) => setDescription (e.target.value) }/>
+            <input  type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount (e.target.value)  }   />
+            <input  type="category" placeholder="Category" value={category} onChange={(e) => setCategory (e.target.value)}  />
+            <button type="submit">Add Expense</button>
+
+        </form>
+    );
     }
